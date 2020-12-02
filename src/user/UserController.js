d39d0730bfe4f9d90a9aca0253435fa234c7395a
Darius Vallejo <dario.vallejo@rappi.com>
2020-12-02 16:50:48 -0500

    solving error

--- src/user/UserController.js
+++ src/user/UserController.js
@@ -11,6 +11,12 @@ class UserController {
     return ClientDAO.associateLikeForPetsByClient(dogId);
   };
 
+  median = (arr) => {
+    const mid = Math.floor(arr.length / 2),
+      nums = [...arr].sort((a, b) => a - b);
+    return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+  };
+
   getSuggestions = async (clientId = 1) => {
     const client = await ClientDAO.getClientById(clientId);
     const allDogs = await ClientDAO.getAllDogs();
@@ -19,32 +25,36 @@ class UserController {
       throw new Error('Lo sentimos no tiene preferencias');
     }
 
+    console.log({ c: client.clientPreference });
+
     const dogsAccumulated = allDogs.reduce(
       (accumulated, dog) => {
         if (dog.clients.length > 0) {
-          return {
-            size: accumulated.size + dog.size,
-            color: accumulated.color + dog.color,
-            area: accumulated.area + dog.area,
+          const a = {
+            size: [...accumulated.size, dog.size],
+            color: [...accumulated.color, dog.color],
+            area: [...accumulated.area, dog.area],
             counter: accumulated.counter + 1,
           };
+          console.log(a);
+          return a;
         }
 
         return accumulated;
       },
       {
-        size: client.clientPreference.size,
-        color: client.clientPreference.color,
-        area: client.clientPreference.area,
+        size: [client.clientPreference.size],
+        color: allDogs.length > 0 ? [] : [client.clientPreference.color],
+        area: [client.clientPreference.area],
         counter: 1,
       },
     );
 
     const y = [
-      dogsAccumulated.size / dogsAccumulated.counter,
-      dogsAccumulated.color / dogsAccumulated.counter,
-      dogsAccumulated.area / dogsAccumulated.counter,
-      1,
+      this.median(dogsAccumulated.size),
+      this.median(dogsAccumulated.color),
+      this.median(dogsAccumulated.area),
+      100,
     ];
 
     console.log({ y, dogsAccumulated });
@@ -54,10 +64,8 @@ class UserController {
         dog.size,
         dog.color,
         dog.area,
-        dog.clients.length > 0 ? 1 : -100,
+        dog.clients.length > 0 ? 100 : -100,
       ];
-      const yLike = [1];
-      const xLike = [dog.clients.length > 0 ? 1 : -100];
       const rate = similarity(x, y);
       return {
         ...dog.dataValues,
